187e89c0c6d467ee666af4fe6f44eea74e7b9234
Mamun Rashid <mamun001@gmail.com>
2022-08-02 18:01:09 -0700

    2 more

---
+++ if_then_else.java
@@ -0,0 +1,18 @@
+
+// Tested on jdoodle
+
+// CLASS
+public class Main {
+
+       public static void main(String args[]) {
+          int x = 30;
+
+          if( x < 20 ) {
+             System.out.print("x is less than 20");
+          }else {
+             System.out.print("x is NOT less than 20");
+          }
+       }
+    }
+
+
